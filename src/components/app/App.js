61296d6d640ff752be3e8e126960d21f8e95406e
zarias2020 <62381691+zarias2020@users.noreply.github.com>
2021-10-11 17:45:15 +0300

    Ленивая загрузка

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,16 @@
+import { lazy, Suspense } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
+import Spinner from '../spinner/Spinner';
 import AppHeader from '../appHeader/AppHeader';
-import { ComicsPage, MainPage, Page404, SingleComicPage } from '../pages';
+// import { ComicsPage, MainPage, Page404, SingleComicPage } from '../pages';
 
+
+// динамические импорты
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+const Page404 = lazy(() => import('../pages/404'));
 
 const App = () => {
 
@@ -11,23 +19,25 @@ const App = () => {
             <div className="app">
                 <AppHeader />
                 <main>
-                    <Switch>
-                        <Route exact path='/'>
-                            <MainPage />
-                        </Route>
+                    <Suspense fallback={<Spinner />}>
+                        <Switch>
+                            <Route exact path='/'>
+                                <MainPage />
+                            </Route>
 
-                        <Route exact path='/comics'>
-                            <ComicsPage />
-                        </Route>
+                            <Route exact path='/comics'>
+                                <ComicsPage />
+                            </Route>
 
-                        <Route exact path='/comics/:comicId'>
-                            <SingleComicPage />
-                        </Route>
+                            <Route exact path='/comics/:comicId'>
+                                <SingleComicPage />
+                            </Route>
 
-                        <Route exact path='*'>
-                            <Page404 />
-                        </Route>
-                    </Switch>
+                            <Route exact path='*'>
+                                <Page404 />
+                            </Route>
+                        </Switch>
+                    </Suspense>
                 </main>
             </div>
         </HashRouter>
